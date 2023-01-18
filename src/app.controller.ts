f746be1e86363ade8205a57810830f343ae2b120
Miras Kubash <miras.kubash@gmail.com>
2023-01-18 18:35:47 +0600

    integrated with kaspi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import {KaspiResponse} from './types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("/parse-product/:productId")
-  parseProduct(@Param('productId') productId:number): string {
-    return this.appService.parseProduct(productId).toString();
+  parseProduct(@Param('productId') productId:number): Promise<KaspiResponse>  {
+    return this.appService.parseProduct(productId);
   }
 
   @Get("/parse-min-price/:productId")
-  parseMinPrice(@Param('productId') productId:number): string {
-    return this.appService.parseMinPrice(productId).toString();
+  parseMinPrice(@Param('productId') productId:number): Promise<number> {
+    return this.appService.parseMinPrice(productId);
   }
 
-  @Get("/parse-seller-spot/:sellerId")
-  parseSellerSpot(@Param('sellerId') sellerId:number): string {
-    return this.appService.parseSellerSpot(sellerId).toString();
+  @Get("/parse-product/:productId/parse-seller-spot/:sellerId")
+  parseSellerSpot(@Param('sellerId') sellerId:string,@Param('productId') productId: number):  Promise<number> {
+    return this.appService.parseSellerSpot(productId,sellerId);
   }
 }
